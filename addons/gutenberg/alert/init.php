74ba5d3afa50fc72575ed09bfbe490b419e099b8
vvv3 <test.vvv@ukr.net>
2019-01-31 02:42:42 +0200

    Gutenberg Blocks : change init logic

--- addons/gutenberg/alert/init.php
+++ addons/gutenberg/alert/init.php
@@ -7,10 +7,10 @@ defined( 'ABSPATH' ) || exit;
  *
  */
 
-use PBH\Model\Gutenberg;
+use PBH\Model\Abstracts\GutenbergAbstract;
 
 if ( ! class_exists( 'PBH_Alert_Gutenberg' ) ) {
-	class PBH_Alert_Gutenberg extends Gutenberg {
+	class PBH_Alert_Gutenberg extends GutenbergAbstract {
 		
 		public function __construct() {
 			parent::__construct();
@@ -19,51 +19,23 @@ if ( ! class_exists( 'PBH_Alert_Gutenberg' ) ) {
 			$this->dir  = __DIR__;
 			$this->url  = plugins_url( '', __FILE__ );
 			
-			
-			// --------------------------------------------------------------
-			//  Example to enqueue additional assets (plugins such as slick) 
-			// --------------------------------------------------------------
-			/*
-			// enqueue back-end assets only
-			$this->enqueue_block_editor_assets( [
-				[
-					'type'          => 'script',
-					'handle_suffix' => 'slick',
-					'filename'      => 'slack_alert.js',
-					// .. OPTIONAL
-				],
-				[
-					'type'          => 'style',
-					'handle_suffix' => 'slick',
-					'filename'      => 'slack_alert.css',
-					'deps'          => [ 'wp-edit-blocks' ], // OPTIONAL
-					// .. OPTIONAL
-				]
-			] );
-			
-			// load both front-end + back-end assets
-			$this->enqueue_block_assets( [
-				[
-					'type'          => 'script',
-					'target'        => 'frontend', // 'backend', 'frontend', 'all'
-					'handle_suffix' => 'slick',
-					'filename'      => 'slack_alert2.js',
-					'deps'          => [ 'jquery' ], // OPTIONAL
-					'ver'           => 1111,         // OPTIONAL
-					'in_footer'     => true          // OPTIONAL
-				],
-				[
-					'type'          => 'style',
-					'target'        => 'frontend', // 'backend', 'frontend', 'all'
-					'handle_suffix' => 'slick',
-					'filename'      => 'slack_alert2.css',
-					'deps'          => [],   // OPTIONAL,  [] - frontend,  [ 'wp-edit-blocks' ] - backend 
-					'ver'           => 1111, // OPTIONAL
-					'media'         => 'all' // OPTIONAL
-				]
-			] );
-			*/
-			
 		}
+		
+		
+		/**
+		 * Register Block Type
+		 */
+		public function register_block_type() {
+			
+			register_block_type( "pbh-blocks/{$this->slug}", [
+				'editor_script' => "pbh-blocks-{$this->slug}-editor", // backend only
+				'editor_style'  => "pbh-blocks-{$this->slug}-editor", // backend only
+				'style'         => "pbh-blocks-{$this->slug}-front",  // backend + frontend
+				'script'        => "pbh-blocks-{$this->slug}-front",  // backend + frontend
+				//'attributes'      => [],               // for dynamic Block
+				//'render_callback' => function () {},   // for dynamic Block
+			] );
+		}
+		
 	}
 }
